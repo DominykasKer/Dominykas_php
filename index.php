c5f6aa0de07f2f93c2b06226be41226dce429df3
Dominykas <dominixas.k@gmail.com>
2018-10-16 14:20:07 +0300

    Netbeans Commit

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo "Woohooo"; ?>
+<!DOCTYPE html>
+<?php
+date_default_timezone_set("Europe/Vilnius");
+?>
+<html>
+   <head>
+       <title> Aš, <?php print date('l') . ' ir PHP' ?>
+       </title>
+   </head>
+
+   <body>
+       <h1> Domas- HTML <?php print 'ir PHP' ?> asas jau nuo <?php print date('2018') . ' metų' ?>  </h1>
+
+       <p> Viskas prasidėjo <?php print date('F') ?> mėnesio, <?php print date('j') ?> dienos 
+
+       </p>
+   </body>
+</html>
+
+
